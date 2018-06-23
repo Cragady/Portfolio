72c500892aa806757738c5e641ae32db534754aa
Cragady <pinkdotdreamer@gmail.com>
2018-06-23 00:53:39 -0600

    added interactive stylistic events

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,1 +1,52 @@
 console.log("Hello, World!");
+
+opacityMouse = function(){
+    $("body").on({
+        
+        
+        
+        mouseenter: function(){
+            selector = $(this).attr("data-unfaded");
+            if(selector === "yes"){
+                return;
+            } else {
+                $(this).find(".portimage").fadeTo("slow", 1);
+                $(this).attr("data-unfaded", "yes");
+            };
+
+        },
+
+        taphold: function(){
+            selector = $(this).attr("data-unfaded");
+            if(selector === "yes"){
+                return;
+            } else {
+                $(this).find(".portimage").fadeTo("slow", 1);
+                $(this).attr("data-unfaded", "yes");
+            }; 
+        },
+
+        mouseleave: function(){
+            if(selector === "yes"){
+                return;
+            } else {
+                $(this).find(".portimage").fadeTo("slow", 0.33);
+            };
+        }
+
+
+    }, "a");
+};
+
+dataResetter = function(){
+    myPorts = $("#my-ports").find("a");
+    myPorts.attr("data-unfaded", "no");
+    setInterval(function(){$(myPorts.attr("data-unfaded", "no"));}, 1500);
+}
+
+
+
+$(document).ready(function(){
+    dataResetter();
+    opacityMouse();
+});
