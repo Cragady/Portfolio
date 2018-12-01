1e0fb74a6b0cbe8f83225472398e6c7d72d5bc1a
Cragady <pinkdotdreamer@gmail.com>
2018-11-30 20:56:27 -0700

    styling overhaul; gave more content to about me; changed code behind lighting up the projects; need to change html with about me

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,52 +1,83 @@
-console.log("Hello, World!");
-
 opacityMouse = function(){
-    $("body").on({
+    let mousePass;
+    $('body').on({
         
         
         
         mouseenter: function(){
-            selector = $(this).attr("data-unfaded");
-            if(selector === "yes"){
+            const selector = $(this).attr('data-unfaded');
+            if(selector === 'yes' && mousePass === 'no'){
+                if($(this).find('.portimage').css('opacity') < '1'){
+                    $(this).find('.portimage').fadeTo(01 ,'1');
+                    $(this).attr('data-unfaded', 'yes');
+                    mousePass = 'skipped';
+                }
                 return;
             } else {
-                $(this).find(".portimage").fadeTo("slow", 1);
-                $(this).attr("data-unfaded", "yes");
+                if($(this).find('.portimage').css('opacity') < '1'){
+                    $(this).find('.portimage').fadeTo(250, 1);
+                    $(this).attr('data-unfaded', 'yes');
+                }
+                return mousePass = selector;
             };
 
         },
 
         taphold: function(){
-            selector = $(this).attr("data-unfaded");
-            if(selector === "yes"){
+            const selector = $(this).attr('data-unfaded');
+            if(selector === 'yes'){
+
                 return;
             } else {
-                $(this).find(".portimage").fadeTo("slow", 1);
-                $(this).attr("data-unfaded", "yes");
+                $(this).find('.portimage').fadeTo(250, 1);
+                $(this).attr('data-unfaded', 'yes');
             }; 
         },
 
         mouseleave: function(){
-            if(selector === "yes"){
-                return;
+            if(mousePass === 'yes'){
+                if($(this).find('.portimage').css('opacity') > '0.33'){
+                    $(this).find('portimage').fadeTo(01, 0.33);
+                    return mousePass = 'no';
+                };
             } else {
-                $(this).find(".portimage").fadeTo("slow", 0.33);
+                if($(this).find('.portimage').css('opacity') > '0.33'){
+                    $(this).find('.portimage').fadeTo(250, 0.33);
+                    return mousePass = 'no';
+                };
             };
         }
 
 
-    }, "a");
+    }, 'a');
 };
 
 dataResetter = function(){
-    myPorts = $("#my-ports").find("a");
-    myPorts.attr("data-unfaded", "no");
-    setInterval(function(){$(myPorts.attr("data-unfaded", "no"));}, 1500);
+    const myPorts = $('#my-ports').find('a');
+    myPorts.attr('data-unfaded', 'no');
+    setInterval(function(){$(myPorts.attr('data-unfaded', 'no'));}, 1500);
 }
 
+aLinker = function(){
+    const pathing = window.location.pathname.split('/');
+    $.each($('a'), function(i, val){
+        if(val.attributes.href.nodeValue === pathing[pathing.length - 1]){
+            $(val).addClass('a-border');
+        };
+    });
+};
 
+diffground = function(){
+    const picArr = ['bamazon.PNG', 'Book-MarkY!.PNG', 'burgers.png', 'Burgers2.PNG', 'chef-in-your-pantry.PNG', 'clicky.PNG', 'friend-finder.PNG', 'friend-finder2.PNG', 'giphy-gifs.PNG', 'hangman-game.PNG', 'intronerd.PNG', 'liriJS.PNG', 'nyt-scrubber.PNG', 'RPG-game.PNG', 'RPS-game.PNG', 'trivia-game.PNG', 'web-scraper.PNG', 'word-guess-pic.PNG'];
+    const prePath = 'assets/images/'
+    const piPath = prePath + picArr[Math.floor(Math.random() * picArr.length)];
+    $('.diffground').css({'background': `url("${piPath}") no-repeat fixed`, 
+    'background-size': '100% 100vh'})
+};
 
 $(document).ready(function(){
+    diffground();
+    aLinker();
     dataResetter();
     opacityMouse();
 });
